82d91a52505a22ea120eb06a704d6e2b490dcb2c
lovrozagar <lovro.zagar5@gmail.com>
2024-10-19 20:55:40 +0200

    chore(shared utils): add & export get optional object helper

--- src/packages/utils/shared/src/index.ts
+++ src/packages/utils/shared/src/index.ts
@@ -1,7 +1,12 @@
-export { cn, type CnOptions } from './cn/cn'
-export { cx, type CxOptions } from './cx/cx'
-export { cva, type VariantProps } from './cva/cva'
 export { chain } from './chain/chain'
-export { noop } from './noop/noop'
-export { polymorphic } from './polymorphic/polymorphic'
+export { cn, type CnOptions } from './cn/cn'
 export { composeEventHandlers } from './compose-event-handlers/compose-event-handlers'
+export { cva, type VariantProps } from './cva/cva'
+export { cx, type CxOptions } from './cx/cx'
+export {
+	EMPTY_OBJECT,
+	getOptionalObject,
+	type OptionalObject,
+} from './get-optional-object/get-optional-object'
+export { noop } from './noop/noop'
+export { polymorphic, type PolymorphicProps } from './polymorphic/polymorphic'
