f8a8834544e8f49eead390b0f40475fbfe15e50c
lovrozagar <lovro.zagar5@gmail.com>
2024-10-26 19:35:44 +0200

    chore(hooks): add index.ts, export hooks & types

---
+++ src/packages/hooks/shared/src/index.ts
@@ -0,0 +1,5 @@
+export { useFreshRef, type UseFreshRefReturn } from "./use-fresh-ref/use-fresh-ref"
+export {
+  useMutationObserver,
+  type UseMutationObserverProps,
+} from "./use-mutation-observer/use-mutation-observer"
