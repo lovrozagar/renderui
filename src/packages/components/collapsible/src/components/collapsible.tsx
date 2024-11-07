7979a9bc6ca431c27d80508aff0ec15e242e39fd
lovrozagar <lovro.zagar5@gmail.com>
2024-11-07 21:37:52 +0100

    chore(repo): remove unnecessary files

---
+++ src/packages/components/collapsible/src/components/collapsible.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import { CollapsibleContent } from "./collapsible-content"
+import { CollapsibleRoot } from "./collapsible-root"
+import { CollapsibleTrigger } from "./collapsible-trigger"
+import { CollapsibleTriggerPrimitive } from "./collapsible-trigger-primitive"
+
+const Collapsible = {
+  Root: CollapsibleRoot,
+  Trigger: CollapsibleTrigger,
+  TriggerPrimitive: CollapsibleTriggerPrimitive,
+  Content: CollapsibleContent
+}
+
+export { Collapsible }
