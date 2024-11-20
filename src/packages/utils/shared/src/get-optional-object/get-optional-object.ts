8b63cde7de02d6a0239a4ad4993d1fef2f4b645f
lovrozagar <lovro.zagar5@gmail.com>
2024-11-20 19:11:11 +0100

    chore(utils): remove unnecessary, instead use directly from radix

--- src/packages/utils/shared/src/get-optional-object/get-optional-object.ts
+++ src/packages/utils/shared/src/get-optional-object/get-optional-object.ts
@@ -3,7 +3,7 @@ type OptionalObject<T extends object> = T | undefined
 const EMPTY_OBJECT = Object.freeze({})
 
 function getOptionalObject<T extends object>(object: OptionalObject<T>) {
-	return object ?? (EMPTY_OBJECT as NonNullable<T>)
+  return object ?? (EMPTY_OBJECT as NonNullable<T>)
 }
 
-export { EMPTY_OBJECT, getOptionalObject, type OptionalObject }
+export { EMPTY_OBJECT, getOptionalObject, getOptionalObject as optional, type OptionalObject }
--- src/packages/utils/shared/src/index.ts
+++ src/packages/utils/shared/src/index.ts
@@ -1,15 +1,14 @@
 export { chain } from "./chain/chain"
 export { cn, type ClassNameProps, type CnOptions } from "./cn/cn"
 export type { Color } from "./color/color"
-export { composeEventHandlers } from "./compose-event-handlers/compose-event-handlers"
 export { cva, type VariantProps } from "./cva/cva"
 export { cx, type CxOptions, type CxReturn } from "./cx/cx"
 export {
   EMPTY_OBJECT,
   getOptionalObject,
-  type OptionalObject
+  optional,
+  type OptionalObject,
 } from "./get-optional-object/get-optional-object"
-export { initializeContext, type InitializeContextProps } from "./initialize-context/initialize-context"
 export { mergeProps } from "./merge-props/merge-props"
 export { noop } from "./noop/noop"
 export { polymorphic, type PolymorphicProps } from "./polymorphic/polymorphic"
