16d7628ea6367575c5ea2c7e4d1a435a4b450c01
lovrozagar <lovro.zagar5@gmail.com>
2024-11-20 19:20:42 +0100

    revert(hooks-internal): controllable state, use directly from radix instead

--- src/packages/hooks/internal/src/index.ts
+++ src/packages/hooks/internal/src/index.ts
@@ -1,3 +1,1 @@
 export { useAriaHandlers, type UseAriaHandlersProps } from "./use-aria-handlers/use-aria-handlers"
-export { useControlledState } from "./use-controlled-state/use-controlled-state"
-export { useUncontrolledState } from "./use-uncontrolled-state/use-uncontrolled-state"
