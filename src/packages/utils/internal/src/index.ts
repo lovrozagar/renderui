a549a07e38124aee69c7642fd999576940d7a58d
lovrozagar <lovro.zagar5@gmail.com>
2024-11-30 11:26:52 +0100

    rever(utils-internal): remove split aria props util

--- src/packages/utils/internal/src/index.ts
+++ src/packages/utils/internal/src/index.ts
@@ -3,5 +3,3 @@ export {
   getAnimationStyleVariables,
   type AnimationStyleVariables
 } from "./get-animation-style-variables/get-animation-style-variables"
-export { splitAriaProps } from "./split-aria-props/split-aria-props"
-
