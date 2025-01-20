e1bdeb297dcc1720636752dbf3038d62b5433235
Lovro Zagar <lovro.zagar5@gmail.com>
2025-01-20 19:18:42 +0100

    refactor(switch): html validity, input conditional render

--- src/packages/components/switch/src/components/switch.tsx
+++ src/packages/components/switch/src/components/switch.tsx
@@ -50,75 +50,77 @@ const Switch = (props: SwitchProps) => {
     ...restInputProps
   } = optional(inputProps)
 
-  const internalContent = (
-    <>
-      <SwitchThumb
-        {...(typeof thumbProps === "function" ? thumbProps({ isChecked: checked }) : thumbProps)}
-      />
-      <span className="sr-only">
-        <input
-          data-slot="switch-input"
-          tabIndex={-1}
-          name={name ?? inputName}
-          className={cn(DEFAULT_HIDDEN_SWITCH_INPUT_CLASSNAME, inputClassName)}
-          value={(inputValue ?? checked) ? "true" : "false"}
-          checked={inputChecked ?? checked}
-          onChange={chain(
-            (event: React.ChangeEvent<HTMLInputElement>) => setChecked(event.target.checked),
-            onChange,
-          )}
-          {...restInputProps}
-        />
-      </span>
-    </>
+  const thumb = (
+    <SwitchThumb
+      {...(typeof thumbProps === "function" ? thumbProps({ isChecked: checked }) : thumbProps)}
+    />
   )
 
   return (
-    <Button
-      role={role}
-      variant={variant}
-      value={checked ? "on" : "off"}
-      aria-checked={checked}
-      aria-disabled={isDisabled}
-      aria-readonly={isReadOnly}
-      aria-invalid={isInvalid}
-      aria-required={isRequired}
-      data-slot="switch"
-      data-state={checked ? "checked" : "unchecked"}
-      data-disabled={isDisabled}
-      data-readonly={isReadOnly}
-      data-invalid={isInvalid}
-      data-required={isRequired}
-      hasRipple={hasRipple}
-      hasDefaultPressedStyles={hasDefaultPressedStyles}
-      className={cx(DEFAULT_SWITCH_CLASSNAME, className)}
-      startContent={
-        typeof startContent === "function"
-          ? ({ isPressed }) => startContent({ isPressed, isChecked: checked })
-          : startContent
-      }
-      endContent={
-        typeof endContent === "function"
-          ? ({ isPressed }) => endContent({ isPressed, isChecked: checked })
-          : endContent
-      }
-      onPress={chain(() => setChecked((previousChecked) => !previousChecked), onPress)}
-      {...restProps}
-    >
-      {typeof children === "function" ? (
-        ({ ...restProps }) => (
+    <>
+      <Button
+        role={role}
+        variant={variant}
+        value={checked ? "on" : "off"}
+        aria-checked={checked}
+        aria-disabled={isDisabled}
+        aria-readonly={isReadOnly}
+        aria-invalid={isInvalid}
+        aria-required={isRequired}
+        data-slot="switch"
+        data-state={checked ? "checked" : "unchecked"}
+        data-disabled={isDisabled}
+        data-readonly={isReadOnly}
+        data-invalid={isInvalid}
+        data-required={isRequired}
+        hasRipple={hasRipple}
+        hasDefaultPressedStyles={hasDefaultPressedStyles}
+        className={cx(DEFAULT_SWITCH_CLASSNAME, className)}
+        startContent={
+          typeof startContent === "function"
+            ? ({ isPressed }) => startContent({ isPressed, isChecked: checked })
+            : startContent
+        }
+        endContent={
+          typeof endContent === "function"
+            ? ({ isPressed }) => endContent({ isPressed, isChecked: checked })
+            : endContent
+        }
+        onPress={chain(() => setChecked((previousChecked) => !previousChecked), onPress)}
+        {...restProps}
+      >
+        {typeof children === "function" ? (
+          ({ ...restProps }) => (
+            <>
+              {children({ ...restProps, isChecked: checked })}
+              {thumb}
+            </>
+          )
+        ) : (
           <>
-            {children({ ...restProps, isChecked: checked })}
-            {internalContent}
+            {children}
+            {thumb}
           </>
-        )
-      ) : (
-        <>
-          {children}
-          {internalContent}
-        </>
-      )}
-    </Button>
+        )}
+      </Button>
+      {(name ?? inputName) ? (
+        <span className="sr-only">
+          <input
+            data-slot="switch-input"
+            tabIndex={-1}
+            name={name ?? inputName}
+            className={cn(DEFAULT_HIDDEN_SWITCH_INPUT_CLASSNAME, inputClassName)}
+            value={(inputValue ?? checked) ? "true" : "false"}
+            checked={inputChecked ?? checked}
+            onChange={chain(
+              (event: React.ChangeEvent<HTMLInputElement>) => setChecked(event.target.checked),
+              onChange,
+            )}
+            {...restInputProps}
+          />
+        </span>
+      ) : null}
+    </>
   )
 }
 
